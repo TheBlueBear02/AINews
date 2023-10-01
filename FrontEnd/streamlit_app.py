c9f7dc49bbb23e7d2dc404b81847475ac5fc5da5
Amir <amir.gabbay@gmail.com>
2023-10-01 16:05:01 +0300

    reports map adding

--- FrontEnd/streamlit_app.py
+++ FrontEnd/streamlit_app.py
@@ -2,28 +2,47 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from geopy.geocoders import Nominatim
+import json
+import folium
+from streamlit_folium import st_folium
 
-# Initialize Nominatim API
-geolocator = Nominatim(user_agent="MyApp")
+# Read the json file and return a dict of it
+#@st.cache_data
+def get_Reports():
+    # Reads the reports that already saves in the database 
+    with open('C:\\Users\\Amir\\development\\projects\\AINews\\Telegram\\AllNews.json') as f: 
+        reportDict = json.load(f)
+    return reportDict
 
-location = geolocator.geocode("Tel Aviv")
 
-lat = location.latitude
-lot = location.longitude
+allJson = get_Reports()
+reports = allJson["Reports"]
+# Creating a sidebar object
+sideB = st.sidebar
 
-df = pd.DataFrame({
-    "col1": lat,
-    "col2": lot,
-    "col3": 200,
-    "col4": np.random.rand(1000, 4).tolist(),
-})
+israelView = (31.78847185,35.21879441094499)
 
-st.map(df,
-    latitude='col1',
-    longitude='col2',
-    size='col3',
-    #color='col4'
-    )
-# Add a selectbox to the sidebar:
-st.sidebar.write('Hello')
+# Creates the map
+map = folium.Map(location=israelView,zoom_start=7,tiles="Cartodb dark_matter")
+
+# Show reports on the map
+for report in reports:
+    if report['Coordinates'] != {}:
+        location = report['Coordinates']['LAT'], report['Coordinates']['LON']
+        folium.Marker(location,popup=report['Text'],icon=folium.Icon(color='red',prefix='fa',icon='star')).add_to(map)
+
+
+
+# Side Bar
+# Header
+sideB.title('Recent Reports')
+sideB.write('-----------------------------')
+
+# Show reports 
+for report in reports:
+    sideB.header(report['Title'])
+    sideB.info(report['Text'])
+    sideB.write(report['PubilshTime'])
+    sideB.write('-----------------------------')
+st_folium(map,width=800,height=800)
 

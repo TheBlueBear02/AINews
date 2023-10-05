0b2f666d0a43eba3a1945db5c66709a8f66f1465
Amir <amir.gabbay@gmail.com>
2023-10-05 20:52:26 +0300

    Improved reports placing on the map

--- FrontEnd/streamlit_app.py
+++ FrontEnd/streamlit_app.py
@@ -26,61 +26,89 @@ def get_Reports():
         reportDict = json.load(f)
     return reportDict
 
-def print_Report(block):
+def print_Report(block,feature_group):
     # Show reports 
     with st.container():
-        # Report Header
-        st.header('❕' + block['Title'] + '🔸', anchor=str(block["Id"]))
         
+        time = block['PublishTime'].split(',')[0]
+        HnM = time.split(':')[0] + ':' +time.split(':')[1]
+        
+        st.text(str(HnM))
+        # Report Header
+        if block['Coordinates']['LAT'] == 0:
+            header = st.subheader('❕' + block['Title'] + '🔸', anchor=str(block["Id"]))
+        elif block['Coordinates']['LAT'] != 0 and block['IsIsrael']:
+            header = st.subheader('🔵 ' + block['Title'] + '🔸', anchor=str(block["Id"]))
+        else:
+            header = st.subheader('🔴 ' + block['Title'] + '🔸', anchor=str(block["Id"]))
+       
         # Report first sentence
         splitReport = block['Text'].split('.')
         st.write(splitReport[0])
-        
+        restReport = ''
         # If Read more button pressed
-        with st.expander("Read More"):
+        with st.expander("Full Report"):
             st.info(block['Text'])
             #map(location=(report['Coordinates']['LAT'], report['Coordinates']['LON']))
             st.write("[Resource↗️](https://web.telegram.org/k/#@hadshotabitahon)")
         # Publish Time
-        st.write(block['PublishTime'])
-        st.write('-----------------------------')
+        
+        st.write('---')
         # Puts the events on the map 
-        if block['Coordinates'] != {}:
+        if block['Coordinates'] != {} and block['Coordinates']['LAT'] != 0:
             location = block['Coordinates']['LAT'], block['Coordinates']['LON']
-            folium.Marker(location,popup=block['Title'],icon=folium.DivIcon(html=reportHtml)).add_to(map)
+            if block['IsIsrael']: 
+                folium.Marker(location,popup=HnM + ' ' + block['Title'],icon=folium.DivIcon(html=reportHtml)).add_to(feature_group)
+            else:
+                folium.Marker(location,popup=HnM + ' ' + block['Title'],icon=folium.DivIcon(html=alertHtml)).add_to(feature_group)
 
-def print_Alert(block):
+        return feature_group
+
+def print_Alert(block,alertMarkers):
     # Show alert 
     # Report Header
         with st.container():
-            st.header('🚨' + block['Title'] + '🔸', anchor=str(block["Id"]))
+           
+            # Publish Time
+            time = block['PublishTime'].split(',')[0]
+            HnM = time.split(':')[0] + ':' +time.split(':')[1]
+            st.text(str(HnM))
+
+            st.header('🚨' + f":red[{block['Title']}]" + '🔸', anchor=str(block["Id"]))
             
 
             st.write(block['Title'])
             
 
             st.write("[Resource↗️](https://web.telegram.org/k/#@CumtaAlertsEnglishChannel)")
-            # Publish Time
-            st.write(block['PublishTime'])
+           
+           
             st.write('-----------------------------')
             
             # Puts the events on the map 
             if block['Places'] != []:
                 for place in block['Places']:
                     location = place['LAT'],place['LON']
-                    folium.Marker(location,popup=block['Type']+ '\n' + block['PublishTime'],icon=folium.DivIcon(html=alertHtml)).add_to(map)
+                    folium.Marker(location,popup=block['Title']+ '\n' + block['PublishTime'],icon=folium.DivIcon(html=alertHtml)).add_to(alertMarkers)
+            return alertMarkers
 
 def print_Feed(reports_cb,alerts_cb):
     # sort the blocks by date time
     sortedFeed = reversed(sorted(feed, key=lambda x: datetime.strptime(x['PublishTime'],'%H:%M:%S, %Y-%m-%d')))
+    reportsMarkers = folium.FeatureGroup(name='Reports')
+    alertMarkers = folium.FeatureGroup(name='Alerts')
 
     for block in sortedFeed:
+        # CALCULATE THE DELTA TIME OF THE BLOCK
+        # IF LESS THAN X PRINT ELSE: DON'T PRINT AND BREAK
         if block['Id'][0] == 'r' and reports_cb:
-           print_Report(block)
+           reportsMarkers = print_Report(block, reportsMarkers)
+
            
         elif block['Id'][0] == 'a' and alerts_cb:
-            print_Alert(block)
-        
+            alertMarkers = print_Alert(block,alertMarkers)
+    reportsMarkers.add_to(map)
+    alertMarkers.add_to(map)
 
 # HTML 
 st.markdown("""
@@ -98,9 +126,10 @@ reportHtml = """
         .report {
         border-radius: 50%;
         background-color: blue;
-        width: 17px;
-        height: 17px;
+        width: 25px;
+        height: 25px;
         position: absolute;
+        size: absolute;
         opacity: 0;
         animation: scaleIn 5s infinite cubic-bezier(.36, .11, .89, .32);
         }                                
@@ -110,7 +139,7 @@ reportHtml = """
             opacity: .5;
         }
         to {
-            transform: scale(3, 3);
+            transform: scale(2, 2);
             opacity: 0;
         }
         } 
@@ -140,20 +169,51 @@ alertHtml = """
             opacity: .5;
         }
         to {
-            transform: scale(3, 3);
+            transform: scale(2, 2);
             opacity: 0;
         }
         } 
         
     </style>
+    
     <div>  
         <div class="alert" style="animation-delay: -3s"></div>                                                                         
         <div class="alert" style="animation-delay: -2s"></div>
         <div class="alert" style="animation-delay: -1s"></div>
-        <div class="alert" style="animation-delay: 0s"></div>      
-                                                                                                               
-    </div>"""
-
+        <div class="alert" style="animation-delay: 0s"></div>                                                                                                           
+    </div>
+    """
+eventHtml = """
+    <style>
+        .event {
+        border-radius: 50%;
+        background-color: orange;
+        width: 17px;
+        height: 17px;
+        position: absolute;
+        opacity: 0;
+        animation: scaleIn 5s infinite cubic-bezier(.36, .11, .89, .32);
+        }                                
+        @keyframes scaleIn {
+        from {
+            transform: scale(.1, .1);
+            opacity: .5;
+        }
+        to {
+            transform: scale(4, 4);
+            opacity: 0;
+        }
+        } 
+        
+    </style>
+    
+    <div>  
+        <div class="event" style="animation-delay: -3s"></div>                                                                         
+        <div class="event" style="animation-delay: -2s"></div>
+        <div class="event" style="animation-delay: -1s"></div>
+        <div class="event" style="animation-delay: 0s"></div>                                                                                                           
+    </div>
+    """
 
 # Creates connection with the google sheets database
 #conn = st.experimental_connection("gsheets", type=GSheetsConnection)
@@ -174,21 +234,39 @@ israelView = (31.78847185,38.21879441094499)
 
 # Creates the map
 map = folium.Map(location=israelView,zoom_start=7.5,tiles="Cartodb dark_matter")
-
-# Get the time
+# Get the time now
 now = datetime.now()
-
 # Side Bar
 with sideB:     
     # Header🔸❕❗
+    
+    # Calculating How many miuntes ago the feed updated
+    current_time = now.strftime("%H:%M")
+    nowStr = datetime.strptime(current_time,"%H:%M")
+    lastUpdate = datetime.strptime(allJson["LastUpdate"],'%H:%M')
+    delta = nowStr - lastUpdate
+    minsAgo = int(delta.total_seconds() / 60)
+   
+    # Ttle
     st.title('Israel Security Dashboard')
-    st.text(now.strftime("%H:%M:%S"))
-
+    
+    # Choosing Feed Data
     reports_cb = st.checkbox('❕ Recent Reports',value=True)
-    alerts_cb = st.checkbox('🚨 Red Alerts',value=True)
-
-    st.text('Last Update: ' + allJson["LastUpdate"])
-    st.write('-----------------------------')
+    alerts_cb = st.checkbox('🚨 Red Alerts')
+    
+    with st.expander("Report Kinds"):
+        st.text('❕ Report not on map')
+        st.text('🚨 Red Alert')
+        st.text('🔵 Israeli Operation on map')
+        st.text('🔴 Terror Attack on map')
+    # Print Last Update
+    if minsAgo < 1:
+        st.text('Just Updated Now')
+    elif minsAgo > 60:
+        st.text('Updated ' + str(int(minsAgo/60)) + 'h Ago')
+    else:
+        st.text('Updated ' + str(minsAgo) + 'm Ago')
+    st.write('---------')
     
     #  Feed
     print_Feed(reports_cb,alerts_cb)
--- Telegram/telethonLearning.py
+++ Telegram/telethonLearning.py
@@ -13,6 +13,86 @@ api_id = keys.telegramID
 api_hash = keys.telegramHash
 openai.api_key = keys.openaiKey
 
+# Get: GPT answer
+# Reutrn: The place's coordinates
+def find_Coordinates(infoToJson):
+    # Initialize Nominatim API
+    geolocator = Nominatim(user_agent="MyApp")
+    try:
+        # Getting the coordinates of the place in Hebrew
+        locationHeb = geolocator.geocode(infoToJson['PlaceHebrew'])
+        # If the coordinates are in resunable area
+        if locationHeb.latitude > 20 and locationHeb.longitude < 40:
+            coordinates = {'LAT': locationHeb.latitude, 'LON':locationHeb.longitude}
+        else:
+            coordinates = {'LAT': 0, 'LON':0}
+        # If the resualt of the above is 0
+        if coordinates["LAT"] == 0:
+            # Getting the coordinates of the place in Eng
+            locationENG = geolocator.geocode(infoToJson['Place'])
+            coordinates = {'LAT': locationENG.latitude, 'LON':locationENG.longitude}
+            if locationENG.latitude > 20 and locationENG.longitude < 40:
+                coordinates = {'LAT': 0, 'LON':0}
+    except:
+        coordinates = {'LAT': 0, 'LON':0}
+    return coordinates
+
+# Creating a dict for each message we want to insert to the json file 
+# Giving the chat gpt the report in Hebrew and he translate it to English and getting useful information from the report
+def save_Message(reportDict,gpt_Instructions,message):
+    isText = True
+    # set the time zone of the messeges to israel's time zone
+    tz = pytz.timezone('Asia/Riyadh') 
+    # Hendeling the time str
+    reportTime = message.date.astimezone(tz)
+    reportTime = reportTime.strftime("%H:%M:%S, %Y-%m-%d")
+    
+    text = str(message.text).replace('חדשות הביטחון****', '')
+    text = text.replace('דובר צה"ל:', '')
+
+    #clean_message_text = re.sub("[^א-ת ]", "", text)
+    gpt_Instructions.append({"role": "user", "content": text})
+
+ 
+    # Call the chatGPT Client
+    complation = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages = gpt_Instructions,
+            temperature = 0
+        )   
+    info = complation.choices[0].message.content
+    # Try to load the gpt json into dict
+    try: 
+        infoToJson = json.loads(info)
+    except:
+        print("Not A Text Message")
+        isText = False
+    if isText:  
+        # Getting the report's coordinates
+        coordinates = find_Coordinates(infoToJson)
+            
+        temp = {
+                "Id": 'r'+str(message.id),
+                "Title": infoToJson['Title'],
+                "Text": infoToJson['Text'],
+                "Place": infoToJson['Place'],
+                "Coordinates": coordinates,
+                "IsIsrael": infoToJson['Attacker'],
+                "Casualties": infoToJson['Casualties'],
+                "PublishTime": reportTime,
+                "Child_Reports" : []
+                }
+        if message.message != '':
+            reportDict['Reports'].insert(0,temp)
+            print('Added Report')
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
+        reportDict['LastUpdate'] = current_time
+        # Overide the json file with the new reports
+        with open('Telegram/AllNews.json', 'w') as f:
+            json.dump(reportDict, f,indent=2)
+    
+    
 """
 Get: the Telegram client and group
 Do: Reads the channel latests reports and save them in a json file
@@ -27,84 +107,25 @@ def get_TelegramReports(group, cl):
     new_posts = cl.iter_messages( 
         entity=group,
         limit = 30,
-        min_id = reportDict["Reports"][0]["Id"], #change the min_id to the last id that saved in the json file
+        min_id = int(reportDict["Reports"][0]["Id"][1:]), #change the min_id to the last id that saved in the json file
+        #min_id = 4933,
         reverse=True
     )
-    
+    # GPT Instructions
     gpt_Instructions = [
     {"role": "system", "content":"You are an israeli news worker, you get a war report and you need to return this info (if mention):A translation of the Hebrew report exactly as given,  Where the event took place, what country or organization  did the action and how many injuries or kills there was "},
     {"role": "system", "content":"Pls responde only with the info you understood from the report there no need to be nice or write other things"},
-    {"role": "system", "content":'Pls return the info in a JSON format. Here is the expected JSON format: {"Title": A short title for the report IN ENGLISH, "Text": The translation of the report from Hebrew to English, "Place": the most specific place of the report in one word maximum 2, "Attacker": the army or terror organization that pulled the action,"Casualties": how many kills/injured people in number from the report for example 2 killed 0 Injured}'},
+    {"role": "system", "content":'Pls return the info in a JSON format. Here is the expected JSON format: {"Title": A short title for the report IN ENGLISH, "Text": The translation of the report from Hebrew to English, "Place": the most specific town or city of the report in one word maximum 2 in English, "Attacker": If israel or israeli forces mentioned write here True if not write False,"Casualties": how many kills/injured people in number from the report for example 2 killed 0 Injured, "PlaceHebrew": the most specific place of the report in one word maximum 2 in Hebrew }'},
     {"role": "system", "content":"DO NOT RETURN ANY INFORMATION IN A DIFFERENT LANGUAGE THAN ENGLISH"},
     {"role": "system", "content":"If some of the data don't mention just write Not entioned"},
     ]
 
 
-    # set the time zone of the messeges to israel's time zone
-    tz = pytz.timezone('Asia/Riyadh') 
-    
-    
     # Creating a dict for each message we want to insert to the json file 
     # Giving the chat gpt the report in Hebrew and he translate it to English and getting useful information from the report
-    
     for message in new_posts: 
-        # Hendeling the time str
-        reportTime = message.date.astimezone(tz)
-        reportTime = reportTime.strftime("%H:%M:%S, %Y-%m-%d")
-        
-        text = str(message.text).replace('חדשות הביטחון****', '')
-        text = text.replace('דובר צה"ל:', '')
-
-        #clean_message_text = re.sub("[^א-ת ]", "", text)
+        save_Message(reportDict,gpt_Instructions,message)
     
-        gpt_Instructions.append({"role": "user", "content": text})
-
-        # Initialize Nominatim API
-        geolocator = Nominatim(user_agent="MyApp")
-
-
-        complation = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages = gpt_Instructions,
-                temperature = 0
-            )   
-        info = complation.choices[0].message.content
-        infoToJson = json.loads(info)
-        
-        try:
-            location = geolocator.geocode(infoToJson['Place'])
-            lat = location.latitude
-            lon = location.longitude
-            if lon > 20:
-                coordinates = {'LAT': lat, 'LON':lon}
-            else:
-                coordinates = {'LAT': 0, 'LON':0}
-        except:
-            coordinates = {'LAT': 0, 'LON':0}
-            print('no place')
-        temp = {
-                "Id": 'r'+str(message.id),
-                "Title": infoToJson['Title'],
-                "Text": infoToJson['Text'],
-                "Place": infoToJson['Place'],
-                "Coordinates": coordinates,
-                "Attacker": infoToJson['Attacker'],
-                "Casualties": infoToJson['Casualties'],
-                "PublishTime": reportTime,
-                "Child_Reports" : []
-                }
-        reportDict['Reports'].insert(0,temp)
-        print('Added Report')
-    
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    print("Update Time:", current_time)  
-    reportDict['LastUpdate'] = current_time
-    # Overide the json file with the new reports
-    with open('Telegram/AllNews.json', 'w') as f:
-        json.dump(reportDict, f,indent=2) 
-    
-
 
 """
 Get: the Telegram client and group
@@ -123,7 +144,7 @@ def get_RedAlerts(group, cl):
     new_alerts = cl.iter_messages( 
             entity=group,
             limit = 20,
-            min_id = reportDict["Alerts"][0]["Id"], #change the min_id to the last id that saved in the json file
+            min_id = int(reportDict["Alerts"][0]["Id"][1:]), #change the min_id to the last id that saved in the json file
             #min_id = 9350,
             reverse=True
         )
@@ -180,7 +201,7 @@ def get_RedAlerts(group, cl):
 with TelegramClient('session_name', api_id, api_hash) as client:
     
     # News
-    news = 'חדשות הביטחון'         # The title of the channel we want to read from
+    news = 'hadshotabitahon'         # The title of the channel we want to read from
     newschannel = client(GetFullChannelRequest(news))
 
     get_TelegramReports(newschannel.full_chat, client)
